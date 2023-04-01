2837c606d204d19981784cda7d3171b1212a646e
youngentry <gentry_@naver.com>
2023-04-02 01:04:33 +0900

    feat(SignUp): 회원가입 기능 구현 01

--- src/pages/sign-up/SignUp.js
+++ src/pages/sign-up/SignUp.js
@@ -1,19 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./sign-up.scss";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+  const URI = process.env.REACT_APP_API_URI;
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isValidEmail, setIsValidEmail] = useState(true);
+  const [isValidPassword, setIsValidPassword] = useState(true);
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+    setIsValidEmail(event.target.value.includes("@"));
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+    setIsValidPassword(event.target.value.length >= 8);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (isValidEmail && isValidPassword) {
+      // 이메일과 비밀번호가 모두 유효할 때에만 서버로 요청을 보냅니다.
+      try {
+        const response = await axios.post(`${URI}/auth/signup`, { email, password });
+        console.log(response);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
   return (
     <div className="sign-up">
-      <form className="sign-up-form__form">
-        <label className="sign-up-form__label">
+      <form className="form" onSubmit={handleSubmit}>
+        <label className="label">
           이메일
-          <input className="sign-up-form__input" type="email" data-testid="email-input" required />
+          <input className="input" data-testid="email-input" type="email" value={email} onChange={handleEmailChange} required />
         </label>
-        <label className="sign-up-form__label">
+        {!isValidEmail && <p>올바른 이메일 형식이 아닙니다.</p>}
+        <label className="label">
           비밀번호
-          <input className="sign-up-form__input" type="password" data-testid="password-input" required />
+          <input className="input" data-testid="password-input" type="password" value={password} onChange={handlePasswordChange} required />
         </label>
-        <button className="sign-up-form__submit-btn" type="submit" data-testid="signup-button">
+        {!isValidPassword && <p>비밀번호는 8자리 이상이어야 합니다.</p>}
+        <button className="submit-btn" data-testid="signup-button" type="submit" disabled={!isValidEmail || !isValidPassword}>
           회원가입
         </button>
       </form>
