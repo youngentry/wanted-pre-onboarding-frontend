5041391ef3877d699978ed12850baaae79800836
youngentry <gentry_@naver.com>
2023-04-04 19:51:22 +0900

    feat(todo): todo리스트 post요청으로 db에 추가

--- src/pages/todo/Todo.js
+++ src/pages/todo/Todo.js
@@ -1,11 +1,35 @@
 import React, { useEffect, useState } from "react";
-import { validateAccessToken } from "../../module/handleAccessToken";
+import { getAccessTokenData, validateAccessToken } from "../../module/handleAccessToken";
 import { useNavigate } from "react-router-dom";
 import "./todo.scss";
+import axios from "axios";
+import { API_BASE_URL } from "../../constants";
 
 const Todo = () => {
   const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useState();
+
+  const [todo, setTodo] = useState("");
+
+  const handleCreateInputChange = (event) => {
+    setTodo(event.target.value);
+  };
+
+  const createTodo = async (event) => {
+    event.preventDefault();
+    const { accessToken } = getAccessTokenData();
+    const data = { todo };
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+    };
+    try {
+      const response = await axios.post(`${API_BASE_URL}/todos`, data, { headers });
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
     const isValidAccessToken = validateAccessToken();
@@ -18,11 +42,13 @@ const Todo = () => {
   return (
     <>
       {isLoggedIn && (
-        <dvi className="todo">
+        <div className="todo">
           <h2 className="title">투두페이지</h2>
-          <form action="">
-            <input data-testid="new-todo-input" />
-            <button data-testid="new-todo-add-button">추가</button>
+          <form onSubmit={createTodo}>
+            <input data-testid="new-todo-input" type="text" value={todo} onChange={handleCreateInputChange} required />
+            <button data-testid="new-todo-add-button" type="submit">
+              추가
+            </button>
           </form>
           <ul className="list">
             <li>
@@ -34,7 +60,7 @@ const Todo = () => {
               <button data-testid="delete-button">삭제</button>
             </li>
           </ul>
-        </dvi>
+        </div>
       )}
     </>
   );
