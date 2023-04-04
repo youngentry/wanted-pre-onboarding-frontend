4bf5f453bc6631922e96c5bc08ef3dcc222a1099
youngentry <gentry_@naver.com>
2023-04-04 19:14:09 +0900

    feat(todo): todo페이지 마크업

--- src/pages/todo/Todo.js
+++ src/pages/todo/Todo.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { validateAccessToken } from "../../module/handleAccessToken";
 import { useNavigate } from "react-router-dom";
+import "./todo.scss";
 
 const Todo = () => {
   const navigate = useNavigate();
@@ -14,7 +15,29 @@ const Todo = () => {
     }
   }, [navigate]);
 
-  return <div>{isLoggedIn && <h2>투두페이지</h2>}</div>;
+  return (
+    <>
+      {isLoggedIn && (
+        <dvi className="todo">
+          <h2 className="title">투두페이지</h2>
+          <form action="">
+            <input data-testid="new-todo-input" />
+            <button data-testid="new-todo-add-button">추가</button>
+          </form>
+          <ul className="list">
+            <li>
+              <label>
+                <input type="checkbox" />
+                <span>TODO 1</span>
+              </label>
+              <button data-testid="modify-button">수정</button>
+              <button data-testid="delete-button">삭제</button>
+            </li>
+          </ul>
+        </dvi>
+      )}
+    </>
+  );
 };
 
 export default Todo;
