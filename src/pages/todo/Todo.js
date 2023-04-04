f9559e889f038e5202789efd1ecf6131c8dfa995
youngentry <gentry_@naver.com>
2023-04-05 00:15:24 +0900

    refactor(todo): 추가 버튼을 눌러 새로운 투두 리스트 추가

--- src/pages/todo/Todo.js
+++ src/pages/todo/Todo.js
@@ -7,18 +7,19 @@ import { API_BASE_URL } from "../../constants";
 
 const Todo = () => {
   const navigate = useNavigate();
+
   const [isLoggedIn, setIsLoggedIn] = useState();
+  const [createTodoInput, setCreateTodoInput] = useState("");
+  const [todoList, setTodoList] = useState([]);
 
-  const [todo, setTodo] = useState("");
-
-  const handleCreateInputChange = (event) => {
-    setTodo(event.target.value);
+  const handleCreateTodoInputChange = (event) => {
+    setCreateTodoInput(event.target.value);
   };
 
-  const createTodo = async (event) => {
+  const submitTodo = async (event) => {
     event.preventDefault();
     const { accessToken } = getAccessTokenData();
-    const data = { todo };
+    const data = { createTodoInput };
     const headers = {
       Authorization: `Bearer ${accessToken}`,
       "Content-Type": "application/json",
@@ -26,6 +27,8 @@ const Todo = () => {
     try {
       const response = await axios.post(`${API_BASE_URL}/todos`, data, { headers });
       console.log(response);
+      setTodoList([...todoList, response.data]);
+      setCreateTodoInput("");
     } catch (error) {
       console.error(error);
     }
@@ -39,26 +42,46 @@ const Todo = () => {
     }
   }, [navigate]);
 
+  useEffect(() => {
+    (async () => {
+      const { accessToken } = getAccessTokenData();
+      const headers = {
+        Authorization: `Bearer ${accessToken}`,
+      };
+      const response = await axios.get(`${API_BASE_URL}/todos`, { headers });
+      console.log(response);
+      setTodoList([...response.data]);
+    })();
+  }, []);
+
   return (
     <>
       {isLoggedIn && (
         <div className="todo">
           <h2 className="title">투두페이지</h2>
-          <form onSubmit={createTodo}>
-            <input data-testid="new-todo-input" type="text" value={todo} onChange={handleCreateInputChange} required />
-            <button data-testid="new-todo-add-button" type="submit">
-              추가
-            </button>
-          </form>
+          <div>
+            <form onSubmit={submitTodo}>
+              <input data-testid="new-todo-input" type="text" value={createTodoInput} onChange={handleCreateTodoInputChange} required />
+              <button data-testid="new-todo-add-button" type="submit">
+                추가
+              </button>
+            </form>
+          </div>
           <ul className="list">
-            <li>
-              <label>
-                <input type="checkbox" />
-                <span>TODO 1</span>
-              </label>
-              <button data-testid="modify-button">수정</button>
-              <button data-testid="delete-button">삭제</button>
-            </li>
+            {todoList.map((data) => {
+              return (
+                <li key={data.id}>
+                  <label>
+                    <input type="checkbox" />
+                  </label>
+                  <div>
+                    <span>{data.todo}</span>
+                    <button data-testid="modify-button">수정</button>
+                    <button data-testid="delete-button">삭제</button>
+                  </div>
+                </li>
+              );
+            })}
           </ul>
         </div>
       )}
