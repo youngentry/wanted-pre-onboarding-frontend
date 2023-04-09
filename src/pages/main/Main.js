a0ef009955557e385e1994fef2b05fd6458b5af3
youngentry <gentry_@naver.com>
2023-04-09 16:29:37 +0900

    feat(Main): hash router 기본 경로 수정

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Main.scss";
+import { useNavigate } from "react-router-dom";
 
 const Main = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/#");
+  }, [navigate]);
+
   return (
     <div className="main">
       <h2>메인</h2>
